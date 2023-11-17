c421452be5220b6bced949c72e8ebe863bcef739
XueshiQiao <qiaoxueshi@gmail.com>
2023-11-17 15:14:27 +0800

    Update UI, show basic infomation in tabs

--- ui/src/components/BasicMediaInfo.vue
+++ ui/src/components/BasicMediaInfo.vue
@@ -21,31 +21,31 @@ export default {
   data() {
     return {
       descriptionColumn: { xxl: 2, xl: 2, lg: 2, md: 1, sm: 1, xs: 1 },
+      activeKey: ref("0"),
     };
   },
   methods: {},
 };
 </script>
+
 <template>
-  <a-flex gap="middle" vertical>
-    <!-- check whether formatInfo is empty:  -->
-    <a-card title="Basic Media Info" v-if="Object.keys(formatInfo).length > 0">
+  <a-tabs v-model:activeKey="activeKey">
+    <a-tab-pane key="0" tab="Basic Media Info" v-if="Object.keys(formatInfo).length > 0">
       <a-descriptions title="" bordered size="small" :column="descriptionColumn">
         <!-- iterator all key values in mediaInfo['format'] -->
-        <a-descriptions-item v-for="(value, key) in formatInfo" :key="key" :label="key">{{
-          value
-        }}</a-descriptions-item>
+        <a-descriptions-item v-for="(value, key) in formatInfo" :key="key" :label="key">
+          {{ value }}
+        </a-descriptions-item>
       </a-descriptions>
-    </a-card>
-
-    <template v-for="stream in streamsInfo" key="stream.index">
-      <a-card :title="`Stream ${stream['index']} : ${stream['codec_type']}`">
+    </a-tab-pane>
+    <template v-for="stream in streamsInfo" :key="`Stream ${stream['index']} : ${stream['codec_type']}`">
+      <a-tab-pane :tab="`Stream ${stream['index']} : ${stream['codec_type']}`">
         <a-descriptions title="" bordered size="small" :column="descriptionColumn">
-          <a-descriptions-item v-for="(value, key) in stream" :key="key" :label="key">{{
-            value
-          }}</a-descriptions-item>
+          <a-descriptions-item v-for="(value, key) in stream" :key="key" :label="key">
+            {{ value }}
+          </a-descriptions-item>
         </a-descriptions>
-      </a-card>
+      </a-tab-pane>
     </template>
-  </a-flex>
+  </a-tabs>
 </template>
