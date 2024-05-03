6c98714e7e34ba97504c876b78d92d17086ef23f
XueshiQiao <qiaoxueshi@gmail.com>
2024-05-03 17:52:50 +0800

    Add CodecDetail.vue component for displaying codec details

---
+++ ui_codec/src/components/CodecDetail.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+  <a-descriptions title="" bordered>
+    <a-descriptions-item label="name">{{ codecInfo.codec.name }} ({{ codecInfo.codec.longName }})</a-descriptions-item>
+    <!-- all debug placeholer -->
+    <a-descriptions-item label="Billing Mode">Prepaid</a-descriptions-item>
+    <a-descriptions-item label="Automatic Renewal">YES</a-descriptions-item>
+    <a-descriptions-item label="Order time">2018-04-24 18:00:00</a-descriptions-item>
+    <a-descriptions-item label="Usage Time" :span="2">2019-04-24 18:00:00</a-descriptions-item>
+    <a-descriptions-item label="Status" :span="3">
+      <a-badge status="processing" text="Running" />
+    </a-descriptions-item>
+    <a-descriptions-item label="Negotiated Amount">$80.00</a-descriptions-item>
+    <a-descriptions-item label="Discount">$20.00</a-descriptions-item>
+    <a-descriptions-item label="Official Receipts">$60.00</a-descriptions-item>
+    <a-descriptions-item label="Config Info">
+      Data disk type: MongoDB
+      <br />
+      Database version: 3.4
+      <br />
+      Package: dds.mongo.mid
+      <br />
+      Storage space: 10 GB
+      <br />
+      Replication factor: 3
+      <br />
+      Region: East China 1
+      <br />
+    </a-descriptions-item>
+  </a-descriptions>
+</template>
+
+
+<script>
+export default {
+  props: {
+    codecInfo: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  created() {
+		console.log("CodecsDetail.vue created, codec info: ", this.codecInfo);
+  },
+  watch: {
+  },
+  data() {
+    return {
+			codecDetailInfo: ref({}),
+    };
+  },
+  methods: {
+    checkOptions(codec) {
+      console.log("Codecs.vue checkOptions, codec: ", codec);
+      // vscode.postMessage({
+      //   type: "show_codec_detail",
+      //   isEncoder: codec.is_encoder,
+      //   codecName: codec.codec_name,
+      // });
+    },
+  },
+};
+</script>
--- ui_codec/src/components/Codecs.vue
+++ ui_codec/src/components/Codecs.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from "vue";
 import { SearchOutlined } from "@ant-design/icons-vue";
+import CodecDetail from "./CodecDetail.vue";
 </script>
 
 <template>
@@ -54,6 +55,16 @@ import { SearchOutlined } from "@ant-design/icons-vue";
       </template>
     </a-table>
   </a-flex>
+
+  <a-modal
+    v-model:open="presentCodecInfo"
+    title="Codec Info"
+    width="70%"
+    wrap-class-name="full-modal"
+    @ok="dismissCodecInfo"
+  >
+    <CodecDetail :codecInfo="currentCodecDetail" />
+  </a-modal>
 </template>
 
 <script>
@@ -72,6 +83,25 @@ export default {
   created() {
     console.log("Codecs.vue created, codecs: ", this.codecs.length);
     this.filteredCodecs = ref(JSON.parse(JSON.stringify(this.codecs)));
+
+		window.addEventListener("message", async (e) => {
+      const { type, body, requestId } = e.data;
+      console.log("Codecs.vue Receive ", type, " message from vscode extension, body: ", body);
+      switch (type) {
+        case "init": {
+          console.log("Codecs.vue hello world from vscode extension, body: ", body);
+          return;
+        }
+
+				case "show_codec_detail": {
+					console.log("Codecs.vue receive show_codec_detail body:", body);
+          this.currentCodecDetail = this.parseDataToJSON(body);
+          console.log("Codecs.vue parseCodecDetail, codec: ", this.currentCodecDetail);
+          this.presentCodecInfo = true;
+					return;
+				};
+      }
+		});
   },
   watch: {
     // on codecTypeOption changed, refilter codecs
@@ -144,6 +174,8 @@ export default {
       ]),
       selectedCodecTypeOption: ref("-"),
       searchText: ref(""),
+      presentCodecInfo: ref(false),
+      currentCodecDetail: ref({}),
     };
   },
   methods: {
@@ -180,11 +212,160 @@ export default {
     },
     checkOptions(codec) {
       console.log("Codecs.vue checkOptions, codec: ", codec);
+      this.currentCodec = codec;
       vscode.postMessage({
         type: "show_codec_detail",
         isEncoder: codec.is_encoder,
         codecName: codec.codec_name,
       });
+    },
+    dismissCodecInfo() {
+      this.presentCodecInfo = false;
+      this.currentCodecDetail = {};
+    },
+
+    ////
+
+    getValuesFromLine(line) {
+      const keyValues = line.split(': ')
+      if (keyValues.length < 2) {
+        return []
+      }
+
+      const values = keyValues[1].split(' ').filter(function(str) {
+        return str.length > 0
+      })
+      return values;
+    },
+
+    /*
+    ffmpeg/libavutil/opt.c:
+
+    static void opt_list(void *obj, void *av_log_obj, const char *unit,
+
+    av_log(av_log_obj, AV_LOG_INFO, "%c%c%c%c%c%c%c%c%c%c%c",
+    (opt->flags & AV_OPT_FLAG_ENCODING_PARAM)  ? 'E' : '.',
+    (opt->flags & AV_OPT_FLAG_DECODING_PARAM)  ? 'D' : '.',
+    (opt->flags & AV_OPT_FLAG_FILTERING_PARAM) ? 'F' : '.',
+    (opt->flags & AV_OPT_FLAG_VIDEO_PARAM)     ? 'V' : '.',
+    (opt->flags & AV_OPT_FLAG_AUDIO_PARAM)     ? 'A' : '.',
+    (opt->flags & AV_OPT_FLAG_SUBTITLE_PARAM)  ? 'S' : '.',
+    (opt->flags & AV_OPT_FLAG_EXPORT)          ? 'X' : '.',
+    (opt->flags & AV_OPT_FLAG_READONLY)        ? 'R' : '.',
+    (opt->flags & AV_OPT_FLAG_BSF_PARAM)       ? 'B' : '.',
+    (opt->flags & AV_OPT_FLAG_RUNTIME_PARAM)   ? 'T' : '.',
+    (opt->flags & AV_OPT_FLAG_DEPRECATED)      ? 'P' : '.');
+    */
+    parseFlags(flagsString) {
+      let mapping = {
+        'E': "Encoding Param",
+        'D': "Decoding Param",
+        'F': "Filtering Param",
+        'V': "Video Param",
+        'A': "Audio Param",
+        'S': "Subtitle Param",
+        'X': "Export",
+        'R': "Readonly",
+        'B': "BSF Param",
+        'T': "Runtime Param",
+        'P': "Deprecated",
+      }
+
+      var flags = []
+      for (const char of flagsString) {
+        const flag = mapping[char]
+        if (flag) {
+          flags.push(flag);
+        }
+      }
+      return flags;
+    },
+
+    // FFmpeg src ref: ffmpeg/fftools/opt_common.c: static void print_codec(const AVCodec *c)
+    parseDataToJSON(dataString) {
+      const lines = dataString.split('\n');
+      const result = {
+        codec:                    {},
+        generalCapabilities:      [],
+        threadingCapabilities:    [],
+        supportedPixelFormats:    [],
+        supportedFramerates:      [],
+        supportedHardwareDevices: [],
+        supportedSampleFormats:   [],
+        supportedSampleRates:     [],
+        supportedChannelLayouts:  [],
+        AVOptions: [],
+      };
+
+      /*
+      matches 3 cases:
+      variance        1            E..V....... Variance based Aq
+      default                      E..V....... Improve resiliency against losses of whole frames
+      psnr            0            E..V.......
+      */
+      let optionExtractRegex = /^(?!-)(\S+)\s+(\S*)\s+([EDFVASXRBTP\.]+)\s*(.*)/
+
+      for (const line_ of lines) {
+        const line = line_.trim();
+        // hit: "Encoder libaom-av1 [libaom AV1]:"
+        if (line.startsWith('Encoder') || line.startsWith("Decoder")) {
+          const [, type, name, longName] = line.match(/([ED][ne]coder) (.+) \[(.*)\]:/);
+          result.codec = {
+            type: type.toLowerCase(),
+            name,
+            longName
+          };
+        } else if (line.startsWith('General capabilities:')) {
+          result.generalCapabilities = this.getValuesFromLine(line);
+        } else if (line.startsWith('Threading capabilities:')) {
+          result.threadingCapabilities = this.getValuesFromLine(line);
+        } else if (line.startsWith('Supported pixel formats:')) {
+          result.supportedPixelFormats = this.getValuesFromLine(line).sort();
+        } else if (line.startsWith('Supported framerates:')) {
+          result.supportedFramerates = this.getValuesFromLine(line)
+        } else if (line.startsWith('Supported sample rates:')) {
+          result.supportedSampleRates = this.getValuesFromLine(line)
+        } else if (line.startsWith('Supported sample formats:')) {
+          result.supportedSampleFormats = this.getValuesFromLine(line)
+        } else if (line.startsWith('Supported hardware devices:')) {
+          result.supportedHardwareDevices = this.getValuesFromLine(line)
+        } else if (line.startsWith('Supported channel layouts:')) {
+          result.supportedChannelLayouts = this.getValuesFromLine(line)
+
+          // matches line: "libaom-av1 encoder AVOptions:"
+        } else if (/.*\sAVOptions:\s*/.test(line)) {
+          // ignore
+          continue;
+
+          // matches flag lines like: "-cpu-used          <int>        E..V....... Quality/Speed ratio modifier (from 0 to 8) (default 1)"
+        } else if (line.startsWith('-')) {
+          let [, name, type, flags, description] = line.match(/\s*(-\S+)\s+(\S+)\s+([EDFVASXRBTP\.]+)\s+(.*)/)
+          flags = this.parseFlags(flags);
+          result.AVOptions.push({
+            name,
+            type,
+            flags,
+            description,
+            options: []
+          })
+          // match flag option value lines like: "good            0            E..V....... Good quality"
+        } else if (optionExtractRegex.test(line)) {
+          let [, name, value, flags, description] = line.match(optionExtractRegex)
+          flags = this.parseFlags(flags);
+          result.AVOptions[result.AVOptions.length-1].options.push({
+            name,
+            value,
+            flags,
+            description
+          })
+        } else {
+          if (line.length > 0) {
+            console.error("unrecognized line: ", line, ", codec name: ", result.encoder)
+          }
+        }
+      }
+
+      return result;
     },
   },
 };
